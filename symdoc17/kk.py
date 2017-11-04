d67db4898e76dd032a9f159f19c7d482dc6e2df4
Shimizu07190 <4mizu.tt@gmail.com>
2017-11-04 20:59:34 +0900

    document完成

--- symdoc17/kk.py
+++ symdoc17/kk.py
@@ -97,17 +97,20 @@ def warshallFloyd(cons_matrix):
 
     return distance_matrix
 
-def draw_graph(cons_matrix,P,n):
-	fig, ax = plt.subplots()
-	for i in range(n):
-		ax.plot(P[i,0],P[i,1], "ro")
+def draw_graph(cons_matrix,P,n,name):
+	figname = 'pics/' + name + '.png'
+	for i in range(n-1):
 		for j in range(i,n):
-			if(cons_matrix[i][j] == 1):
-				ax.plot([P[i,0],P[j,0]],[P[i,1],P[j,1]],'k-')
-	#plt.axes().set_aspect('equal','datalim')
-	plt.show()
+			if(cons_matrix[i,j] == 1):
+				plt.plot([P[i,0],P[j,0]],[P[i,1],P[j,1]],'k-')
+	plt.plot(P.T[0],P.T[1],'go')
+	plt.title(name)
+	plt.axes().set_aspect('equal', 'datalim')
+	plt.savefig(figname)
+	plt.clf()
+	#plt.show()
 
-def kk_algo(vlis, conslis, L0=10, K0=10, eps=0.001):
+def kk_algo(vlis, conslis, name, L0=10, K0=10, eps=0.0001):
 	n = len(vlis)
 	cons_matrix = makeMaterix(vlis,conslis)
 	D = warshallFloyd(cons_matrix)
@@ -123,46 +126,27 @@ def kk_algo(vlis, conslis, L0=10, K0=10, eps=0.001):
 	print(P)
 	#print(K)
 	#print(L)
-	draw_graph(cons_matrix,P,n)
+	draw_graph(cons_matrix,P,n, name+"_default")
 	delta_max = 10000.0
 	while (delta_max > eps):
 		Maxi, delta_max = [max for max in check_max(P, L, K, n)]
 		delata_p = cal_loop(slice_P(P,Maxi), L[Maxi], K[Maxi], P[Maxi], n)
 		P[Maxi] = P[Maxi] + delata_p
-	draw_graph(cons_matrix,P,n)
+	draw_graph(cons_matrix,P,n, name+"_moved")
 	return P
 
-@doit
-def test_kk1():
-	lis = [0,1,2,3]
-	conslis = [[0,1,2],[2,3]]
-	P = kk_algo(lis,conslis)
+def test_KK(lis,conslis,name):
+	P = kk_algo(lis,conslis,name)
 	print(P)
-
-@doit
-def test_kk2():
-	lis = [0,1,2,3,4,5]
-	conslis = [[0,1],[0,2],[0,3],[0,4],[1,2],[1,4],[1,5],[2,3],[2,5],[3,4],[3,5],[4,5]]
-	P = kk_algo(lis,conslis)
-	print(P)
-
-@doit
-def test_kk3():
-	lis = [0,1,2,3]
-	conslis = [[0,2],[1,2],[0,3],[1,3]]
-	P = kk_algo(lis,conslis)
-	print(P)
-
-@doit
-def test_kk4():
-	lis = [0,1,2,3,4,5,6,7,8,9,10,11]
-	conslis = [[0,1],[5,7],[6,7],[7,8],[6,8],[8,9],[9,10],[10,11],[8,11]]
-	P = kk_algo(lis,conslis)
-	print(P)
+	markdown(
+r'''
+![最適化前](pics/{name}_default.png)
+![最適化後](pics/{name}_moved.png)
+''', **locals())
 
 @doit
 def __document__():
-	n = sp.Symbol('n' , integer = True)
+	n = sp.Symbol('n',integer = True)
 	# ある頂点iについて考えるとする
 	# よってここのIndexedBaseはlambdifyした後にiとの差を受け取ることになる
 
@@ -178,12 +162,14 @@ def __document__():
 	length_j = sp.sqrt(sp.Sum((Pi[j,d]-p[d])**2, d_range)).doit()
 	potential_j = Ki[j] * (length_j - Li[j]) ** 2 /2
 	function_E = sp.Sum(potential_j,j_range)
-	GRAD_0 = sp.simplify(sp.diff(function_E,p[0]))
-	GRAD_1 = sp.simplify(sp.diff(function_E,p[1]))
-	HESS_00 = sp.simplify(sp.diff(GRAD_0,p[0]))
-	HESS_01 = sp.simplify(sp.diff(GRAD_0,p[1]))
-	HESS_10 = sp.simplify(sp.diff(GRAD_1,p[0]))
-	HESS_11 = sp.simplify(sp.diff(GRAD_1,p[1]))
+	E = sp.Function('E')
+	dEp0 = sp.diff(E(p[0],p[1]), p[0])
+	dEp1 = sp.diff(E(p[0],p[1]), p[1])
+	dEp00 = sp.diff(dEp0, p[0])
+	dEp01 = sp.diff(dEp0, p[1])
+	dEp10 = sp.diff(dEp1, p[0])
+	dEp11 = sp.diff(dEp1, p[1])
+	delta_i = sp.sqrt(dEp0**2 + dEp1**2)
 	markdown(
 r'''
 # KK法による無向グラフの描画
@@ -195,17 +181,50 @@ r'''
 $${{length}}_j={length_j}$$
 で定義される。$v_i$と$v_j$をつなぐバネのポテンシャルはその自然長を$L_j$、バネ係数を$K_j$とすると
 $${{potential_j}}={potential_j}$$
-と定義できる。よって、$v_i$においてのポテンシャルは、
-$${{function_E}}={function_E}$$
+と定義できる。よって、$v_i$においてのポテンシャル$E$は、
+$${{E}}={function_E}$$
 バネの自然長は、頂点$v_i$と$v_j$のグラフ上の距離を$d_{{i,j}}$として
-$$L_j=L_0 * d_{{i,j}}$$と定める。また、
+$$L_j=L_0 \times d_{{i,j}}$$と定める。また、バネ係数はそれぞれ
+$$K_j=K_0 / d_{{i,j}}^2$$と定める。$K_0$と$L_0$は定数である。
+次に、このエネルギーを最小化していき、安定な状態を求める。
+そのために、$p=P_i$と定め、$p$だけを変数とみて、考える。
+この$p$は各$v_i$の中で
+$$\Delta_i = {delta_i}$$
+が最大の$i$とする。そして、$p$の新しい座標を
+$$p_0 = p_0 + \delta p_0$$
+$$p_1 = p_1 + \delta p_1$$
+とする。この$\delta p_0$、$\delta p_1$は次の線形方程式の解である
+$${dEp00} \delta p_0 + {dEp01} \delta p_1 = -{dEp0}$$
+$${dEp10} \delta p_0 + {dEp11} \delta p_1 = -{dEp1}$$
+これを繰り返し、$\Delta_i$の最大値が十分小さくなるまで繰り返す。
+
 
 
 ''',**locals())
+lis1 = [0,1,2,3]
+conslis1 = [[0,1,2],[2,3]]
+lis2 = [0,1,2,3,4,5]
+conslis2 = [[0,1],[0,2],[0,3],[0,4],[1,2],[1,4],[1,5],[2,3],[2,5],[3,4],[3,5],[4,5]]
+lis3 = [0,1,2,3,4,5,6,7,8,9,10,11]
+conslis3 = [[0,1],[1,5],[2,3,4,5],[5,7],[6,7],[7,8],[6,8],[8,9],[9,10],[10,11],[8,11]]
+lis4 =[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+conslis4 = [[0,1,2,3],[3,4],[4,5,6,7],[4,8],[8,9,10,11],[8,12],[12,13,14,15],[3,12]]
+lis5 = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
+conslis5 = [[0,1],[0,4],[0,5],[1,2],[1,7],[2,3],[2,9],[3,4],[3,11],[4,13],[5,6],[5,14],[6,7],[6,15],[7,8],[8,9],[8,16],[9,10],[10,11],[10,17],[11,12],[12,13],[12,18],[13,14],[14,19],[15,16],[15,19],[16,17],[17,18],[18,19]]
 
+test_KK(lis1,conslis1,"test1")
+test_KK(lis2,conslis2,"test2")
+test_KK(lis3,conslis3,"test3")
+test_KK(lis4,conslis4,"test4")
+test_KK(lis5,conslis5,"test5")
 
 
 '''
+	GRAD_0_s = "\frac{{\partial E}}{{\partial p_0}}"
+	GRAD_1_s = "\frac{{\partial E}}{{\partial p_1}}"
+	HESS_00_s = "\frac{{\partial^2 E}}{{\partial^2 p_0}}"
+	HESS_00_s = "\frac{{\partial^2 E}}{{\partial p_0 \partial p_1}}"
+	HESS_00_s = "\frac{{\partial^2 E}}{{\partial^2 p_1}}"
 def type_check(sym_object):
 	print(type(sym_object))
 	
